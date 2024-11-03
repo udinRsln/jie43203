5634db84a0df99c46485a8d4fcabad6fbc547fb9
udinRsln <93848502+udinRsln@users.noreply.github.com>
2024-11-03 13:21:22 +0800

    Update project_ds.py

--- project_ds.py
+++ project_ds.py
@@ -7,4 +7,18 @@ st.write(df)
 # Checking for Null Values
 df.isnull().sum()
 
+# HANDLE OUTLIERS FOR ADULT MORTALITY COL
+import matplotlib.pyplot as plt
+# Creating dataset
+data=df['Adult Mortality']
+
+fig = plt.figure(figsize =(10, 7))
+# Creating plot
+plt.boxplot(data)
+# show plot
+plt.show()
+st.ppyplot(plt.gcf())
+# Now you can use plt to create figures and plots:
+fig = plt.figure(figsize=(10, 7))
+
 
