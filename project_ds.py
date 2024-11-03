e93f892bbdba45f01e4cdf71aee6788b16aae8fa
udinRsln <93848502+udinRsln@users.noreply.github.com>
2024-11-03 13:07:32 +0800

    Update project_ds.py

--- project_ds.py
+++ project_ds.py
@@ -4,4 +4,17 @@ import pandas as pd
 df = pd.read_csv('https://raw.githubusercontent.com/udinRsln/jie43203/refs/heads/main/Life%20Expectancy%20Data.csv')
 st.write(df)
 
+# HANDLE OUTLIERS FOR ADULT MORTALITY COL
+import matplotlib.pyplot as plt
+# Creating dataset
+data=df['Adult Mortality']
+
+fig = plt.figure(figsize =(10, 7))
+# Creating plot
+plt.boxplot(data)
+# show plot
+plt.show()
+# Now you can use plt to create figures and plots:
+fig = plt.figure(figsize=(10, 7))
+
 
