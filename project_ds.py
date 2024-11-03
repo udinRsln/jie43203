ee6fb14f3a5996a893a5b76c016f912eaa4d2114
udinRsln <93848502+udinRsln@users.noreply.github.com>
2024-11-03 13:10:38 +0800

    Update project_ds.py

--- project_ds.py
+++ project_ds.py
@@ -4,10 +4,19 @@ import pandas as pd
 df = pd.read_csv('https://raw.githubusercontent.com/udinRsln/jie43203/refs/heads/main/Life%20Expectancy%20Data.csv')
 st.write(df)
 
+# Checking for Null Values
+data.isnull().sum()
+
+# DELETE UNUSE COLUMN
+df = pd.DataFrame(data)
+del df['GDP']
+
+df.dropna(inplace=True)
+
 # HANDLE OUTLIERS FOR ADULT MORTALITY COL
 import matplotlib.pyplot as plt
 # Creating dataset
-df=['Adult Mortality']
+data=df['Adult Mortality']
 
 fig = plt.figure(figsize =(10, 7))
 # Creating plot
@@ -16,5 +25,4 @@ plt.boxplot(data)
 plt.show()
 # Now you can use plt to create figures and plots:
 fig = plt.figure(figsize=(10, 7))
-
 
