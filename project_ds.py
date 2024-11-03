ee11e2ea0a98f927000cbb5bb7cf70f2602c58ad
udinRsln <93848502+udinRsln@users.noreply.github.com>
2024-11-03 13:03:56 +0800

    Update project_ds.py

--- project_ds.py
+++ project_ds.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-df = pd.read_csv('/content/Life Expectancy Data.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/udinRsln/jie43203/refs/heads/main/Life%20Expectancy%20Data.csv')
 st.write(df)
 
 
