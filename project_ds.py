45ddb3b1f446a2eaaad51c08dda816c78c54f06f
udinRsln <93848502+udinRsln@users.noreply.github.com>
2024-11-10 08:59:12 +0800

    Update project_ds.py

--- project_ds.py
+++ project_ds.py
@@ -4,11 +4,13 @@ import pandas as pd
 df = pd.read_csv('https://raw.githubusercontent.com/udinRsln/jie43203/refs/heads/main/Life%20Expectancy%20Data.csv')
 st.write(df)
 
-from sklearn.metrics import mean_squared_error, mean_absolute_error,  r2_score
-print("mse: ", mean_squared_error(y_test, y_pred))
-print("mae: ", mean_absolute_error(y_test, y_pred))
-
-# r2 score for model performance
-r2 = r2_score(y_test, y_pred)
-print(f'R-squared(model performance): {r2}')
+try:
+    print("MSE: ", mean_squared_error(y_test, y_pred))
+    print("MAE: ", mean_absolute_error(y_test, y_pred))
+    
+    # r2 score for model performance
+    r2 = r2_score(y_test, y_pred)
+    print(f'R-squared (model performance): {r2}')
+except NameError as e:
+    print(f"Error: {e}. Pastikan 'y_test' dan 'y_pred' sudah didefinisikan.")
 
